1ed6ea1bd63fec0e7311e5a94f8c4f7d1b3bfc41
Matyasmester <matyas.zoboki@gmail.com>
2024-04-22 21:17:37 +0200

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Minesweeper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RestartButton = new System.Windows.Forms.Button();
+            this.TimeRemainingLabel = new System.Windows.Forms.Label();
+            this.ClicksLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.RestartButton.Location = new System.Drawing.Point(350, 12);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(147, 62);
+            this.RestartButton.TabIndex = 0;
+            this.RestartButton.Text = "Restart";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // TimeRemainingLabel
+            // 
+            this.TimeRemainingLabel.AutoSize = true;
+            this.TimeRemainingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TimeRemainingLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.TimeRemainingLabel.Location = new System.Drawing.Point(42, 9);
+            this.TimeRemainingLabel.Name = "TimeRemainingLabel";
+            this.TimeRemainingLabel.Size = new System.Drawing.Size(63, 69);
+            this.TimeRemainingLabel.TabIndex = 1;
+            this.TimeRemainingLabel.Text = "0";
+            // 
+            // ClicksLabel
+            // 
+            this.ClicksLabel.AutoSize = true;
+            this.ClicksLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ClicksLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.ClicksLabel.Location = new System.Drawing.Point(752, 9);
+            this.ClicksLabel.Name = "ClicksLabel";
+            this.ClicksLabel.Size = new System.Drawing.Size(63, 69);
+            this.ClicksLabel.TabIndex = 2;
+            this.ClicksLabel.Text = "0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(868, 648);
+            this.Controls.Add(this.ClicksLabel);
+            this.Controls.Add(this.TimeRemainingLabel);
+            this.Controls.Add(this.RestartButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Minesweeper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RestartButton;
+        private System.Windows.Forms.Label TimeRemainingLabel;
+        private System.Windows.Forms.Label ClicksLabel;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Minesweeper
+{
+    public partial class MainForm : Form
+    {
+        private Graphics graphics;
+
+        private const int rectUnit = 25;
+        private const int topOverlayEndsAt = 75;
+        private const int nMaxBombs = 10;
+
+        private const char Clear = 'C';
+        private const char Bomb = 'B';
+        private const char Discovered = 'D';
+
+        private readonly Size rectSize = new Size(rectUnit, rectUnit);
+
+        private readonly Pen blackPen = new Pen(Color.Black, 2);
+        private readonly Pen darkGrayPen = new Pen(Color.DarkGray, 2);
+        private readonly Brush bgColorBrush;
+        private readonly Brush grayBrush = new SolidBrush(Color.Gray);
+
+        private readonly Random random = new Random();
+
+        private readonly List<Label> addedLabels = new List<Label>();
+
+        private char[,] Map;
+
+        private int nClicks = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            graphics = this.CreateGraphics();
+
+            Map = new char[this.Width, this.Height];
+
+            this.Shown += new EventHandler(this.MainForm_Shown);
+
+            this.MouseClick += onMouseClick;
+
+            bgColorBrush = new SolidBrush(this.BackColor);
+        }
+
+        private void onMouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Y < topOverlayEndsAt) return;
+
+            if(e.Button == MouseButtons.Left)
+            {
+                ClicksLabel.Text = (++nClicks).ToString();
+
+                Point clampedLocation = new Point();
+
+                clampedLocation.X = (int)Math.Floor(e.X / (decimal)rectUnit) * rectUnit;
+                clampedLocation.Y = (int)Math.Floor(e.Y / (decimal)rectUnit) * rectUnit;
+
+                int x = clampedLocation.X;
+                int y = clampedLocation.Y;
+
+                if (Map[x, y] == Bomb)
+                {
+                    if(nClicks == 1)
+                    {
+                        ReplaceBomb(x, y);
+                        return;
+                    }
+                    else { MessageBox.Show("Meghaltal xdd balfasz"); }
+                }
+
+                DiscoverCellsFrom(x, y);
+            }
+        }
+
+        private void CreateAndDrawGrid()
+        {
+            for (int x = 0; x < this.Width; x += rectUnit)
+            {
+                for (int y = topOverlayEndsAt; y < this.Height; y += rectUnit)
+                {
+                    Rectangle rect = new Rectangle(new Point(x, y), rectSize);
+
+                    graphics.FillRectangle(bgColorBrush, rect);
+                    graphics.DrawRectangle(blackPen, rect);
+
+                    Map[x, y] = Clear;
+                }
+            }
+
+            PlaceBombs();
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            CreateAndDrawGrid();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlaceBombs()
+        {
+            for (int i = nMaxBombs; i > 0; i--)
+            {
+                int randX, randY;
+
+                do
+                {
+                    randX = random.Next(0, this.Width / rectUnit) * rectUnit;
+                    randY = random.Next(topOverlayEndsAt / rectUnit, (this.Height / rectUnit) - 1) * rectUnit;
+                }
+                while (Map[randX, randY] == Bomb);
+
+                Map[randX, randY] = Bomb;
+
+                graphics.FillRectangle(new SolidBrush(Color.Red), new Rectangle(new Point(randX, randY), rectSize));
+            }
+        }
+
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            CreateAndDrawGrid();
+
+            foreach(Label label in addedLabels)
+            {
+                this.Controls.Remove(label);
+            }
+        }
+
+        private void ReplaceBomb(int x, int y)
+        {
+            Map[x, y] = Clear;
+
+            int randX, randY;
+
+            do
+            {
+                randX = random.Next(0, this.Width / rectUnit) * rectUnit;
+                randY = random.Next(topOverlayEndsAt / rectUnit, (this.Height / rectUnit) - 1) * rectUnit;
+            }
+            while (Map[randX, randY] == Bomb || (randX == x && randY == y));
+
+            Map[randX, randY] = Bomb;
+        }
+
+        private void DiscoverCellsFrom(int x, int y)
+        {
+            int bombCount = GetAdjacentBombCount(x, y);
+
+            Label label = new Label()
+            {
+                Location = new Point(x + 3, y + 2),
+                Text = bombCount.ToString(),
+                Width = 15,
+                Height = 18,
+                Font = new Font("Arial", 14, FontStyle.Bold)
+            };
+
+            switch (bombCount)
+            {
+                case 1:
+                    label.ForeColor = Color.Blue;
+                    break;
+                case 2:
+                    label.ForeColor = Color.Green;
+                    break;
+                case 3:
+                    label.ForeColor = Color.Red;
+                    break;
+                case 4:
+                    label.ForeColor = Color.DarkBlue;
+                    break;
+                default:
+                    break;
+            }
+            this.Controls.Add(label);
+
+            addedLabels.Add(label);
+        }
+
+        private int GetAdjacentBombCount(int x, int y)
+        {
+            int bombCount = 0;
+
+            int lowerX = (x - rectUnit) < 0 ? 0 : (x - rectUnit);
+            int lowerY = (y - rectUnit) < 0 ? 0 : (y - rectUnit);
+
+            int higherX = (x + rectUnit) > this.Width ? this.Width : (x + rectUnit);
+            int higherY = (y + rectUnit) > this.Height ? this.Height : (y + rectUnit);
+
+            for (int X = lowerX; X <= higherX; X += rectUnit)
+            {
+                for(int Y = lowerY; Y <= higherY; Y += rectUnit)
+                {
+                    if (X == x && Y == y) continue;
+
+                    if (Map[X, Y] == Bomb) bombCount++;
+                }
+            }
+
+            return bombCount;
+        }
+    }
+}
